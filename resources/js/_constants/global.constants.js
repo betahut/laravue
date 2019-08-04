2808f72092e998379d6031a1163f80ba86ad1aa7
moaj257 <moaj257@gmail.com>
2019-08-04 11:33:34 +0530

    Changed SiteUrl

--- resources/js/_constants/global.constants.js
+++ resources/js/_constants/global.constants.js
@@ -1,5 +1,5 @@
 export const _siteName = 'Explorer';
-export const _siteUrl = 'http://127.0.0.1:8000/';
+export const _siteUrl = 'https://dicus.herokuapp.com/';
 export const _clientIdPersonal = 1;
 export const _clientSecretPersonal = 'OP4PgYDpzhr4PPxRpI5779eKKRL8AAlbdU1gdTJc';
 export const _clientId = 2;
