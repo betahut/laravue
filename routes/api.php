c53dda9abb60b937a84a183acff2efcef3680d77
moaj257 <moaj257@gmail.com>
2019-08-05 21:12:42 +0530

    Added FTP Controller & FTP api routes

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,5 @@
 use Illuminate\Http\Request;
 $api = app('Dingo\Api\Routing\Router');
 $api->version('v1', function ($api) {
-    $api->post('ftp/list', ['as' => 'ftp.list', 'uses' => 'App\Http\Controllers\Api\FtpController@list']);
+    $api->get('ftp/list', ['as' => 'ftp.list', 'uses' => 'App\Http\Controllers\Api\FtpController@list']);
 });
